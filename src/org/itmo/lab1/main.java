17cd149af06ec4622c6c9e59368223e24a3e1108
EvgeniyKvasov <eakvasov@yandex.ru>
2026-01-15 23:18:27 +0300

    Create git repository Java with case 1 lab 1

---
+++ src/org/itmo/lab1/main.java
@@ -0,0 +1,10 @@
+package org.itmo.lab1;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Я");
+        System.out.println("хорошо");
+        System.out.println("знаю");
+        System.out.println("Java.");
+    }
+}
