fbeaf38ebbb57d5414010c4ebfa10c41c17ec398
EvgeniyKvasov <eakvasov@yandex.ru>
2026-01-25 22:10:24 +0300

    Сделано задание 4 части 2 лабораторной 4.

--- src/org/itmo/lab1/lab3/lab4/Case10.java
+++ src/org/itmo/lab1/lab3/lab4/Case10.java
@@ -1,6 +1,39 @@
-package org.itmo.lab1.lab3.lab4;
+import java.util.Arrays;
+
+import java.util.Random;
 
 public class Case10 {
+    public static void main(String[] args){
 
-    // Каркас заданий 1-5 части 2 лабораторной №4.
+        int[] myArray = new int[6];
+
+        Random random = new Random();
+
+        for (int i = 0; i < myArray.length; i++) {
+
+            myArray[i] = random.nextInt(5);
+        }
+
+        System.out.println(Arrays.toString(myArray));
+
+        for (int i = 0; i < myArray.length; i++){
+
+            boolean result = true;
+
+            for (int j = 0; j < myArray.length; j++){
+
+                if (myArray[i] == myArray[j] && i != j){
+
+                    result = false;
+
+                    break;
+                }
+            }
+
+            if (result) System.out.println(myArray[i]);
+
+        }
+
+
+    }
 }
