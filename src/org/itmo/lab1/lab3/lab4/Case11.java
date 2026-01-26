eea526ca224d2c68d9284d76bd61c956717d4b56
EvgeniyKvasov <eakvasov@yandex.ru>
2026-01-26 16:53:49 +0300

    В задании 5 части 2 лабораторной 4 реализовано заполнение массива рандомными числами, разделенеи массива пополам и вывод правой и левой частей массива.

--- src/org/itmo/lab1/lab3/lab4/Case11.java
+++ src/org/itmo/lab1/lab3/lab4/Case11.java
@@ -1,6 +1,39 @@
-package org.itmo.lab1.lab3.lab4;
+import java.util.Arrays;
+
+import java.util.Random;
 
 public class Case11 {
-    //
-    //Каркас заданий 1-5 части 2 лабораторной №4.
+    public static void main(String[] args) {
+
+        int[] myArray = new int[10];
+
+        Random random = new Random();
+
+        for (int i = 0; i < myArray.length; i++) {
+
+            myArray[i] = random.nextInt(20);
+        }
+
+        System.out.println(Arrays.toString(myArray));
+
+        int center = myArray.length / 2;
+
+        int[] left = new int[center];
+
+        int[] right = new int[myArray.length - center];
+
+
+        for (int i = 0; i < myArray.length; i++) {
+            if (i < center) {
+                left[i] = myArray[i];
+            } else {
+                right[i - center] = myArray[i];
+            }
+
+        }
+
+        System.out.println(Arrays.toString(left));
+
+        System.out.println(Arrays.toString(right));
+    }
 }
