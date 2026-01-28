866b542f5e0ba4795f54cba64f5230704b6def1e
EvgeniyKvasov <eakvasov@yandex.ru>
2026-01-28 15:17:57 +0300

    Сделано задание 1.

--- src/org/itmo/lab1/lab3/lab4/lab5/Main.java
+++ src/org/itmo/lab1/lab3/lab4/lab5/Main.java
@@ -4,6 +4,31 @@ public class Main {
 
     public static void main(String[] args){
 
+        System.out.println("Задача 1.");
 
+        System.out.println("Ищу самое длинное слово в этом тексте.");
+
+        String text = "Ищу самое длинное слово в этом тексте";
+
+        String longest = findLongestWord(text);
+
+        System.out.println("Самое длинное слово: " + longest + ".");
+    }
+
+    public static String findLongestWord(String text) {
+
+        String[] words = text.split(" ");
+
+        String longest = "";
+
+        for (String word : words) {
+
+            if (word.length() > longest.length()) {
+
+                longest = word;
+            }
+        }
+
+        return longest;
     }
 }
