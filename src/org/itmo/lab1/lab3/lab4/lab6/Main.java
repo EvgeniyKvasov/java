b5b9c0d3783fd3d02ea206085b8d20c72eabfdf1
EvgeniyKvasov <eakvasov@yandex.ru>
2026-02-01 14:03:03 +0300

    Создан абстрактный класс человек (правки по структуре кода)

--- src/org/itmo/lab1/lab3/lab4/lab6/Main.java
+++ src/org/itmo/lab1/lab3/lab4/lab6/Main.java
@@ -1,30 +1,35 @@
 package org.itmo.lab1.lab3.lab4.lab6;
 
+abstract class Human {
+
+    String firstName;
+
+    String lastName;
+
+    public String getFirstName() {
+
+        return firstName;
+    }
+
+    public String getLastName() {
+
+        return lastName;
+    }
+
+    public abstract void displayInfo();
+
+    public Human(String firstName, String lastName) {
+
+        this.firstName = firstName;
+
+        this.lastName = lastName;
+    }
+}
+
 public class Main {
 
-    abstract class Human {
+    public static void main(String[] args) {
 
-        String firstName;
 
-        String lastName;
-
-        public String getFirstName() {
-
-            return firstName;
-        }
-
-        public String getLastName() {
-
-            return lastName;
-        }
-
-        public abstract void displayInfo();
-
-        public Human(String firstName, String lastName) {
-
-            this.firstName = firstName;
-
-            this.lastName = lastName;
-        }
     }
 }
