ffaa82fe38d758523421fcbc2fd8321d2640ae30
EvgeniyKvasov <eakvasov@yandex.ru>
2026-02-06 12:42:10 +0300

    Выполнена лабораторная 7.

--- src/org/itmo/lab7/Main.java
+++ src/org/itmo/lab7/Main.java
@@ -1,4 +1,37 @@
 package org.itmo.lab7;
 
+
+class Airplane {
+
+    class Wing {
+
+        private int weight;
+
+        public void showWeight() {
+
+            System.out.println("Вес крыла: " + weight + " кг");
+        }
+
+        public Wing(int weight) {
+
+            this.weight = weight;
+        }
+    }
+}
+
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Airplane airplane = new Airplane();
+
+        Airplane.Wing leftWing = airplane.new Wing(500);
+
+        Airplane.Wing rightWing = airplane.new Wing(550);
+
+        leftWing.showWeight();
+
+        rightWing.showWeight();
+    }
 }
