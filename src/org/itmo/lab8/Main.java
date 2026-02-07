d1ffda1a7755210feee1308cfa2c927042395a07
EvgeniyKvasov <eakvasov@yandex.ru>
2026-02-07 14:44:33 +0300

    Сделана задача 2.

--- src/org/itmo/lab8/Main.java
+++ src/org/itmo/lab8/Main.java
@@ -11,6 +11,46 @@ public class Main {
         List<String> list = Arrays.asList("a", "b", "a", "b", "c", "g", "f");
 
         System.out.println(removeDuplicates(list));
+
+        //case2
+
+        List<Integer> arrayList = new ArrayList<>();
+
+        List<Integer> linkedList = new LinkedList<>();
+
+        Random random = new Random();
+
+        for (int i = 0; i < 1000000; i++) {
+
+            arrayList.add(i);
+
+            linkedList.add(i);
+
+        }
+
+        //System.out.println(arrayList);  уууупппссссс;)
+
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i < 100000; i++) {
+
+            arrayList.get(random.nextInt(1000000));
+        }
+
+        long arrayListTime = System.currentTimeMillis() - start;
+
+        System.out.println("ArrayList: " + arrayListTime + " милисекунд.");
+
+        long start1 = System.currentTimeMillis();
+
+        for (int i = 0; i < 100000; i++) {
+
+            linkedList.get(random.nextInt(1000000));
+        }
+
+        long linkedListTime = System.currentTimeMillis() - start1;
+
+        System.out.println("LinkedList: " + linkedListTime + " милисекунд.");
     }
 
     //case1
