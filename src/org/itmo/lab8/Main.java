37d879d84f5b6776a47609199234f2ed48f72b97
EvgeniyKvasov <eakvasov@yandex.ru>
2026-02-07 13:25:58 +0300

    Сделана задача 1.

--- src/org/itmo/lab8/Main.java
+++ src/org/itmo/lab8/Main.java
@@ -1,4 +1,22 @@
 package org.itmo.lab8;
 
+import java.util.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        //case1
+
+        List<String> list = Arrays.asList("a", "b", "a", "b", "c", "g", "f");
+
+        System.out.println(removeDuplicates(list));
+    }
+
+    //case1
+
+    public static <T> Collection<T> removeDuplicates(Collection<T> collection) {
+
+        return new HashSet<>(collection);
+    }
 }
