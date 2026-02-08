964d188e509c678c6813bfb9c752e198ac36b43b
EvgeniyKvasov <eakvasov@yandex.ru>
2026-02-08 09:37:32 +0300

    Сделана задача 2.

--- src/org/itmo/lab9/Main.java
+++ src/org/itmo/lab9/Main.java
@@ -8,6 +8,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //case1
+
         try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
 
             String line;
@@ -31,6 +33,31 @@ public class Main {
         } catch (IOException e) {
 
             System.out.println("Ошибка при чтении файла: " + e.getMessage());
+        }
+
+        //case2
+
+        String text = "Hello World!";
+
+        try {
+
+            writeToFile("output.txt", text);
+
+            System.out.println("Строка " + text + " успешно записана в файл!");
+
+        } catch (IOException e) {
+
+            System.out.println("Ошибка при записи в файл: " + e.getMessage());
+        }
+    }
+
+    //case2
+
+    public static void writeToFile(String filename, String content) throws IOException {
+
+        try (FileWriter writer = new FileWriter(filename)) {
+
+            writer.write(content);
         }
     }
 }
